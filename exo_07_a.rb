5a3926aa8a887bc4f777da758717cd0d9e58c297
Caroleboe <b_carole@live.fr>
2018-06-21 16:12:46 +0200

    Ajout exo 7 a

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+# gets.chomp permet de saisir des donnes provenant de l'utilisateur. En l'occurence ici c'est le nom.

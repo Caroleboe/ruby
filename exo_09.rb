d76455e8dc9a06444acc086e72822a9770e19018
Caroleboe <b_carole@live.fr>
2018-06-21 16:15:02 +0200

    Ajout exo 9

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, quel est votre prénom ?"
+user_name = gets.chomp
+puts user_name
+
+puts "Bonjour, quel est votre nom de famille ?"
+sur_name = gets.chomp
+puts sur_name
+
+puts "Bonjour #{user_name + sur_name}!"

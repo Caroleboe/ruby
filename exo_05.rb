4d7535b0ea3e9146b6265b1ceb8b2b460c0826b4
Caroleboe <b_carole@live.fr>
2018-06-21 16:11:12 +0200

    Ajout exo 5

---
+++ exo_05.rb
@@ -0,0 +1,31 @@
+
+# #{} est utilisé pour faire de la concaténation
+
+# On affiche simplement le texte entre les guillemets
+puts "On va compter le nombre d'heures de travail à THP"
+# On affiche le texte plus le résultat de la multiplication comprise dans les accolades
+puts "Travail : #{10 * 5 * 11}"
+# On affiche le texte plus le résultat de la multiplication comprise dans les accolades
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# On affiche le texte entre guillemets
+puts "Et en secondes ?"
+# on affiche le résultat de la multiplication des 6 nombres
+puts 10 * 5 * 11 * 60 * 60
+# on affiche le texte entre guillemets
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# on demande si 3 plus 2 est plus petit que 5 moins 7. Ce n'est pas le cas donc le terminal indique "false"
+puts 3 + 2 < 5 - 7
+# on affiche le texte entre guillemets et on demande à afficher le résultat de 3 + 2
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# on affiche le texte entre guillements et on demande à afficher le résultats de 5 moins 7
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# on affiche le texte entre guillemets
+puts "Ok, c'est faux alors !"
+# on affiche le texte entre guillemets
+puts "C'est drôle ça, faisons-en plus :"
+# on affiche le texte entre guillemets et on demande si 5 est supérieur que -2. Le Terminal affiche que c'est vrai "true"
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+# on affiche le texte entre guillemets et on demande si 5 est supérieur ou égal à -2. C'est vrai donc le terminal affiche "true" 
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+# on affiche le texte entre guillemets et on demande si 5 est inférieur ou égal à -2. C'est faux donc le terminal affiche "false"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
